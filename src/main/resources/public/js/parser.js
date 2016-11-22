39c2875c982c756c2e5fb17aa974d709ffa17d39
ELEKS-SOFTWARE\andrii.novikov <adnyre@gmail.com>
2016-11-22 13:31:10 +0200

    fix & simplify regex

--- src/main/resources/public/js/parser.js
+++ src/main/resources/public/js/parser.js
@@ -1,6 +1,6 @@
 var replaceUrl = function (t) {
-    var httpExpression = /(^|\s)(https?:\/\/[^\s\.]+\.\.[^\s]+\.[^\s]{2,})/gi;
-    var wwwExpression = /(^|\s)(www\.[^\s]+\.[^\s]{2,})/gi;
+    var httpExpression = /(^|\s)(https?:\/\/[^\s.]+[^\s]+)/gi;
+    var wwwExpression = /(^|\s)(www\.[^\s]+)/gi;
     t = t.replace(httpExpression, '$1<a href="$2" class="matched">$2</a>');
     return t.replace(wwwExpression, '$1<a href="http://$2" class="matched">$2</a>');
 }
